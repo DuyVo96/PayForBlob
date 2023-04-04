2a76b28b73b5de7b9ac84567027180d69965e17d
NiceLife001 <vominhduy.cokhi@gmail.com>
2023-04-04 14:41:34 +0700

    change

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,10 @@ const {
 module.exports = (app) => {
   app.use(
     createProxyMiddleware("/submit_pfb", {
-      target: "http://149.102.158.186:26659",
+      // target: "http://149.102.158.186:26659",
+      target:
+        "https://celestia.onepiece-cosmos-explorer.xyz",
+
       changeOrigin: true,
     })
   );
@@ -13,9 +16,13 @@ module.exports = (app) => {
     createProxyMiddleware(
       "/namespaced_shares/:namespacedShares/height/:height",
       {
-        target: "http://149.102.158.186:26659",
+        // target: "http://149.102.158.186:26659",
+        target:
+          "https://celestia.onepiece-cosmos-explorer.xyz",
         changeOrigin: true,
       }
     )
   );
 };
+
+// https://celestia.onepiece-cosmos-explorer.xyz
